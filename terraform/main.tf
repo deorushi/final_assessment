07be77ab5adc4ab6fbf52b028a5a21f8d04ea99b
Rushikesh Deo <deorushi@gmail.com>
2025-09-05 16:13:45 +0530

    recent_commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 # Backend resources (will switch to remote backend later)
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,13 @@ variable "location" {
   type        = string
   default     = "EastUS"
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
