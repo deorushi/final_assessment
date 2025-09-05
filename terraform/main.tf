df7886828268ea53c9dacb618490554aca3c7fb8
Rushikesh Deo <deorushi@gmail.com>
2025-09-05 14:02:39 +0530

    Third_commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,6 @@ terraform {
     storage_account_name = "tfstateaccount"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-
-    # Explicit SP credentials for backend
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
   }
 }
 
