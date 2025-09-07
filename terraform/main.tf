6ecd203d38f169511b418945b4239fb38a8eb8c3
Rushikesh Deo <deorushi@gmail.com>
2025-09-07 15:41:16 +0530

    ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_security_group_id = azurerm_network_security_group.ssh_nsg.id
 }
 
-# Linux VM
+# Linux VM with full automation
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "ci-cd-vm"
   resource_group_name   = azurerm_resource_group.ci_cd_rg.name
@@ -92,7 +92,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   disable_password_authentication = true
-
+  
   admin_ssh_key {
     username   = var.vm_username
     public_key = file("${path.module}/ci_cd_key.pub")
@@ -110,45 +110,46 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  # -----------------------
-  # Provisioner installs everything
-  # -----------------------
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      host        = azurerm_public_ip.vm_pip.ip_address
-      user        = var.vm_username
-      private_key = file("${path.module}/ci_cd_key")
-    }
+  custom_data = base64encode(<<-EOT
+#!/bin/bash
+set -e
 
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y docker.io curl conntrack socat tar git build-essential golang-go make",
-      "sudo ln -s /usr/sbin/conntrack /usr/local/bin/conntrack || true",
-      "sudo groupadd docker || true",
-      "sudo usermod -aG docker ${var.vm_username}",
-      "sudo systemctl enable docker",
-      "sudo systemctl restart docker",
-      # kubectl
-      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
-      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
-      # minikube
-      "curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-      "sudo install minikube-linux-amd64 /usr/local/bin/minikube",
-      # crictl
-      "CRICTL_VERSION=v1.30.0",
-      "curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz",
-      "sudo tar zxvf crictl-${CRICTL_VERSION}-linux-amd64.tar.gz -C /usr/local/bin",
-      "rm crictl-${CRICTL_VERSION}-linux-amd64.tar.gz",
-      # cri-dockerd
-      "git clone https://github.com/Mirantis/cri-dockerd.git",
-      "cd cri-dockerd",
-      "make cri-dockerd",
-      "sudo cp cri-dockerd /usr/local/bin/",
-      "sudo cp packaging/systemd/* /etc/systemd/system/",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl enable cri-docker.service",
-      "sudo systemctl start cri-docker.service"
-    ]
-  }
+# Update & install basic tools
+apt-get update -y
+apt-get install -y docker.io curl conntrack socat tar git build-essential golang-go make
+
+# Add user to docker group
+usermod -aG docker ${var.vm_username}
+systemctl enable docker
+systemctl restart docker
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Install cri-tools
+CRICTL_VERSION="v1.30.0"
+curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/$$CRICTL_VERSION/crictl-$$CRICTL_VERSION-linux-amd64.tar.gz
+tar zxvf crictl-$$CRICTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm crictl-$$CRICTL_VERSION-linux-amd64.tar.gz
+
+# Install cri-dockerd
+git clone https://github.com/Mirantis/cri-dockerd.git /tmp/cri-dockerd
+cd /tmp/cri-dockerd
+make cri-dockerd
+install cri-dockerd /usr/local/bin/
+cp packaging/systemd/* /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl start cri-docker.service
+cd /
+
+# Create CNI plugin directory (required by minikube)
+mkdir -p /opt/cni/bin
+EOT
+  )
 }
