01d53f5d96a5daaa85d2dbe9c38564d3a49305b7
Rushikesh Deo <deorushi@gmail.com>
2025-09-08 15:29:01 +0530

    alldone

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "aks_api_server" {
 }
 
 output "aks_kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_admin_config[0].raw_kube_config
+  value     = azurerm_kubernetes_cluster.aks.kube_admin_config[0].kube_config
   sensitive = true
 }
 
