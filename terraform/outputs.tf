7e0afdc4982d305612a6871d040227d6cfa271b5
Rushikesh Deo <deorushi@gmail.com>
2025-09-08 14:52:13 +0530

    all_good

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,16 @@
+output "aks_api_server" {
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.host
+}
+
+output "aks_kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_admin_config.raw_kube_config
+  sensitive = true
+}
+
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
+output "aks_resource_group" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
 }
