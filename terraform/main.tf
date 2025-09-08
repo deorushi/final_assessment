c4240b0084a63b6b28060ed734b335a59e59d965
Rushikesh Deo <deorushi@gmail.com>
2025-09-08 11:57:18 +0530

    Done

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,6 +110,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 #!/bin/bash
 set -e
 export DEBIAN_FRONTEND=noninteractive
+CRICTL_VERSION="v1.30.0"
 
 # Basic packages
 apt-get update -y
@@ -131,10 +132,9 @@ install /tmp/minikube /usr/local/bin/minikube
 rm -f /tmp/minikube
 
 # crictl (hardcoded version to avoid Terraform interpolation issues)
-CRICTL_VERSION="v1.30.0"
-curl -Lo /tmp/crictl-${CRICTL_VERSION}.tar.gz "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz"
-tar -C /usr/local/bin -xzf /tmp/crictl-${CRICTL_VERSION}.tar.gz
-rm -f /tmp/crictl-${CRICTL_VERSION}.tar.gz
+curl -Lo /tmp/crictl-$CRICTL_VERSION.tar.gz "https://github.com/kubernetes-sigs/cri-tools/releases/download/$CRICTL_VERSION/crictl-$CRICTL_VERSION-linux-amd64.tar.gz"
+tar -C /usr/local/bin -xzf /tmp/crictl-$CRICTL_VERSION.tar.gz
+rm -f /tmp/crictl-$CRICTL_VERSION.tar.gz
 
 # cri-dockerd   
 git clone https://github.com/Mirantis/cri-dockerd.git /tmp/cri-dockerd
